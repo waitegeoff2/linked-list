cbe6f8f62d62dd16d4a1a51ca24005a561a6096a
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-25 10:43:36 -0400

    Added prepend function

--- linked-list.js
+++ linked-list.js
@@ -1,3 +1,10 @@
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
 class LinkedList {
     constructor() {
         this.head = null;
@@ -5,12 +12,13 @@ class LinkedList {
 
     append(value) {
         //if head is empty, add to list
-        if(this.head == null){
-            this.head == new Node(value);
-        } 
+        if(this.head === null){
+            this.head = new Node(value);
+        }
 
         //add to first one that is null
         let currentNode = this.head;
+
         //while the current node you are on doesn't have a null next to it, keep going
         while(currentNode.nextNode !== null) {
             currentNode = currentNode.nextNode;
@@ -20,22 +28,21 @@ class LinkedList {
     }
 
     prepend(value) {
-        
-    }
-}
-
-class Node {
-    constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-
+        if(this.head == null){
+            this.head = new Node(value);
+        } else if (this.head !== null) {
+            //add node in front of head
+            let currentNode = new Node(value);
+            currentNode.nextNode = this.head;
+            this.head = currentNode;
+        }  
     }
 }
 
 let defaultList = new LinkedList();
 
+defaultList.append("dog");
+defaultList.append("cat");
+defaultList.prepend("monkey");
+
 console.log(defaultList);
-
-let defaultNode = new Node();
-
-console.log(defaultNode);
