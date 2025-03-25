a2c98e5f39b647b21b39f854a519aca3029b841e
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-25 10:03:45 -0400

    add append function

--- linked-list.js
+++ linked-list.js
@@ -1,7 +1,41 @@
-import { Node } from './node';
-
 class LinkedList {
-    constructor(name) {
-        this.name = name;
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        //if head is empty, add to list
+        if(this.head == null){
+            this.head == new Node(value);
+        } 
+
+        //add to first one that is null
+        let currentNode = this.head;
+        //while the current node you are on doesn't have a null next to it, keep going
+        while(currentNode.nextNode !== null) {
+            currentNode = currentNode.nextNode;
+        }
+        //stopped on node with nothing next to it, add node to the next one
+        currentNode.nextNode = new Node(value);
+    }
+
+    prepend(value) {
+        
     }
 }
+
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+
+    }
+}
+
+let defaultList = new LinkedList();
+
+console.log(defaultList);
+
+let defaultNode = new Node();
+
+console.log(defaultNode);
--- node.js
+++
@@ -1,9 +0,0 @@
-class Node {
-    constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-
-    }
-}
-
-export {Node};
